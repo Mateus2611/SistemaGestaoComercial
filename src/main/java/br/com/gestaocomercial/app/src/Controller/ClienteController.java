bae165b3439e0f2c672f829a75871752c02adb52
Mateus Henrique <mateushas.dev@gmail.com>
2026-05-13 18:17:00 -0300

    Finalizando implementação do modulo de Cliente (Repository, Service, DTO, Controller)

--- src/main/java/br/com/gestaocomercial/app/src/Controller/ClienteController.java
+++ src/main/java/br/com/gestaocomercial/app/src/Controller/ClienteController.java
@@ -1,12 +1,18 @@
 package br.com.gestaocomercial.app.src.Controller;
 
 import br.com.gestaocomercial.app.src.Model.Cliente;
+import br.com.gestaocomercial.app.src.Model.DTO.UpdateClienteDTO;
+import br.com.gestaocomercial.app.src.Model.Email;
+import br.com.gestaocomercial.app.src.Model.Endereco;
 import br.com.gestaocomercial.app.src.Service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class ClienteController {
@@ -15,7 +21,24 @@ public class ClienteController {
     private ClienteService _clienteService;
 
     @RequestMapping("/cliente")
-    public Iterable<Cliente> get() {
-        return _clienteService.BuscaGeral();
-    }
+    public Iterable<Cliente> get() { return _clienteService.BuscaGeral(); }
+
+    @RequestMapping("/cliente/{id}")
+    public Cliente getById(@Param("id") Integer id) { return _clienteService.BuscaPorId(id); }
+
+    public Cliente create(Cliente cliente, Endereco endereco, List<Email> emails) { return _clienteService.Criar(cliente, endereco, emails); }
+
+    public Cliente update(UpdateClienteDTO clienteDTO) { return _clienteService.Atualizar(clienteDTO); }
+
+    public Cliente disable(Integer id) { return _clienteService.InativarCliente(id); }
+
+    public Cliente activate(Integer id) { return _clienteService.AtivarCliente(id); }
+
+    public List<Email> addEmails(Integer idCliente, List<Email> emails) { return _clienteService.CriarEmailCliente(idCliente, emails); }
+
+    public void deleteEmail(Integer emailId) { _clienteService.ExcluirEmailCliente(emailId); }
+
+    public List<Email> getClientEmails(Integer clientId) { return _clienteService.ExibirEmailsCliente(clientId); }
+
+
 }
--- src/main/java/br/com/gestaocomercial/app/src/Repository/IClienteRepository.java
+++ src/main/java/br/com/gestaocomercial/app/src/Repository/IClienteRepository.java
@@ -1,9 +1,20 @@
 package br.com.gestaocomercial.app.src.Repository;
 
 import br.com.gestaocomercial.app.src.Model.Cliente;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface IClienteRepository extends CrudRepository<Cliente, Integer> {
+    @Query("UPDATE `Cliente` SET `Status_Cliente` = 'INATIVO', `Data_Inativacao` = :date WHERE `Id` = :id")
+    void Disable(@Param("id") Integer id, @Param("data") Date date);
+
+    @Query("UPDATE `Cliente` SET `Status_Cliente` = 'ATIVO' WHERE `Id` = :id")
+    void Activate(@Param("id") Integer id);
 }
